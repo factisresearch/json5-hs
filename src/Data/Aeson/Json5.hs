e18c48172720ef73acf43148417d49a7356f9430
Timo von Holtz <tvh@tvholtz.de>
2020-12-21 12:40:45 +0200

    First somewhat correct JSON-Parser

--- src/Data/Aeson/Json5.hs
+++ src/Data/Aeson/Json5.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -7,31 +9,75 @@ module Data.Aeson.Json5
   )
 where
 
+import Control.Applicative (Applicative (..))
 import Data.Aeson
+import qualified Data.HashMap.Strict as HM
 import Data.Scientific (Scientific)
+import Data.String
+import qualified Data.Vector as V
 import Data.Void
 import Text.Megaparsec
-import Text.Megaparsec.Char
+import qualified Data.Text as T
+import qualified Text.Megaparsec.Char as C
 import qualified Text.Megaparsec.Char.Lexer as L
 
 parseJson :: StreamContstraint s => s -> Either String Value
 parseJson s =
-  case runParser (space *> jsonP <* space <* eof) "" s of
+  case runParser (jsonP <* jsonFiller <* eof) "" s of
     Left e -> Left (errorBundlePretty e)
     Right x -> Right x
 
-type StreamContstraint s = (Stream s, Token s ~ Char)
+type StreamContstraint s = (Stream s, Token s ~ Char, IsString (Tokens s))
 
 type Parser s a = Parsec Void s a
 
-jsonP :: StreamContstraint s => Parser s Value
-jsonP = primitiveP
+jsonFiller :: StreamContstraint s => Parser s ()
+jsonFiller = C.space
 
-primitiveP :: StreamContstraint s => Parser s Value
-primitiveP =
+jsonP :: StreamContstraint s => Parser s Value
+jsonP =
+  jsonFiller *> jsonP'
+
+jsonP' :: StreamContstraint s => Parser s Value
+jsonP' =
   Number <$> numberP
+    <|> nullP
+    <|> Bool <$> boolP
+    <|> String <$> stringP
+    <|> Array <$> arrayP
+    <|> Object <$> dictP
+
+arrayP :: StreamContstraint s => Parser s Array
+arrayP =
+  let arrayElem = C.char ',' *> jsonP
+      nonEmptyContents = liftA2 (:) jsonP' (many arrayElem) <* jsonFiller
+   in C.char '['
+        *> jsonFiller
+        *> (fmap V.fromList nonEmptyContents <|> pure V.empty) <* C.char ']'
+
+dictP :: StreamContstraint s => Parser s Object
+dictP =
+  let dictKeyPair = do
+        key <- stringP
+        jsonFiller
+        C.char ':'
+        val <- jsonP
+        return (key, val)
+      dictElem = C.char ',' *> jsonFiller *> dictKeyPair
+      nonEmptyContents = liftA2 (:) dictKeyPair (many dictElem) <* jsonFiller
+   in C.char '{'
+        *> jsonFiller
+        *> (fmap HM.fromList nonEmptyContents <|> pure HM.empty) <* C.char '}'
+
+nullP :: StreamContstraint s => Parser s Value
+nullP = C.string "null" *> pure Null
+
+boolP :: StreamContstraint s => Parser s Bool
+boolP = C.string "true" *> pure True <|> C.string "false" *> pure False
 
 numberP :: StreamContstraint s => Parser s Scientific
 numberP =
-  L.signed (return ()) $
-  L.scientific
+  L.signed (return ()) L.scientific
+
+stringP :: StreamContstraint s => Parser s T.Text
+stringP = C.char '"' *> fmap T.pack (manyTill L.charLiteral (C.char '"'))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,59 +1,64 @@
 {-# LANGUAGE TupleSections #-}
 
-import Language.JavaScript.Inline
 import Control.Monad
 import Data.Aeson
 import Data.Aeson.Json5
 import Data.Maybe
+import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.IO as T
+import Language.JavaScript.Inline
 import System.Directory
 import System.FilePath.Posix
 import Test.Tasty
 import Test.Tasty.HUnit
 
-data TestSpec =
-  TestSpec
+data TestSpec = TestSpec
   { jsonShouldParse :: Bool
   }
 
 main :: IO ()
 main =
   withSession defaultConfig $ \session -> do
-  let testDir = "json5-tests"
-  allEntries <- listDirectory testDir
-  allTestGroups <-
-    fmap catMaybes $
-      forM allEntries $ \dir -> do
-        let dir' = testDir </> dir
-        isDirectory <- doesDirectoryExist dir'
-        case isDirectory of
-          False -> return Nothing
-          True -> do
-            allTestFiles <- listDirectory dir'
-            allTests <-
-              fmap catMaybes $
-                forM allTestFiles $ \testFile -> do
-                  let testFile' = dir' </> testFile
-                  let mTestSpec =
-                        case takeExtension testFile' of
-                          ".json" -> Just TestSpec{jsonShouldParse = True}
-                          ".json5" -> Just TestSpec{jsonShouldParse = False}
-                          ".txt" -> Just TestSpec{jsonShouldParse = False}
-                          ".js" -> Just TestSpec{jsonShouldParse = False}
-                          ext -> Nothing
-                  let mkTests testSpec =
-                        let jsonTest = do
-                              contents <- T.readFile testFile'
-                              case (jsonShouldParse testSpec, parseJson contents) of
-                                (False, Left _e) -> return ()
-                                (False, Right x) -> assertFailure $ "Unexpected successful parse:\n" <> show x
-                                (True, Left e) -> assertFailure $ "Failed parse:\n" <> e
-                                (True, Right jsonRes) ->
-                                  case eitherDecodeStrict' (T.encodeUtf8 contents) of
-                                    Left e -> assertFailure $ "Aeson failed to parse:\n" <> e
-                                    Right aesonRes -> assertEqual "" aesonRes jsonRes
-                         in testGroup testFile [testCase "JSON" jsonTest]
-                  return . fmap mkTests $ mTestSpec
-            return . Just $ testGroup dir allTests
-  defaultMain $ testGroup "json5-tests" allTestGroups
+    let testDir = "json5-tests"
+    allEntries <- listDirectory testDir
+    allTestGroups <-
+      fmap catMaybes $
+        forM allEntries $ \dir -> do
+          let dir' = testDir </> dir
+          isDirectory <- doesDirectoryExist dir'
+          case isDirectory of
+            False -> return Nothing
+            True -> do
+              allTestFiles <- listDirectory dir'
+              allTests <-
+                fmap catMaybes $
+                  forM allTestFiles $ \testFile -> do
+                    let testFile' = dir' </> testFile
+                    let mTestSpec =
+                          case takeExtension testFile' of
+                            ".json" -> Just TestSpec {jsonShouldParse = True}
+                            ".json5" -> Just TestSpec {jsonShouldParse = False}
+                            ".txt" -> Just TestSpec {jsonShouldParse = False}
+                            ".js" -> Just TestSpec {jsonShouldParse = False}
+                            ext -> Nothing
+                    let mkTests testSpec =
+                          let jsonTest = do
+                                contents <- T.readFile testFile'
+                                case (jsonShouldParse testSpec, parseJson contents) of
+                                  (False, Left _e) -> return ()
+                                  (False, Right x) ->
+                                    assertFailure $
+                                      "Unexpected successful parse: "
+                                        <> show x
+                                        <> "\nInput:\n"
+                                        <> T.unpack contents
+                                  (True, Left e) -> assertFailure $ "Failed parse:\n" <> e
+                                  (True, Right jsonRes) ->
+                                    case eitherDecodeStrict' (T.encodeUtf8 contents) of
+                                      Left e -> assertFailure $ "Aeson failed to parse:\n" <> e
+                                      Right aesonRes -> assertEqual "" aesonRes jsonRes
+                           in testGroup testFile [testCase "JSON" jsonTest]
+                    return . fmap mkTests $ mTestSpec
+              return . Just $ testGroup dir allTests
+    defaultMain $ testGroup "json5-tests" allTestGroups
