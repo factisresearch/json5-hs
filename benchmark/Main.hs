0a81fb5baf5faebd7016316c4a92d5d1aa513572
Timo von Holtz <tvh@tvholtz.de>
2021-01-11 10:08:05 +0100

    Also test benchmarks for Text-input

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -5,6 +5,7 @@ import Criterion.Main
 import Data.Aeson as Aeson
 import Data.Aeson.Json5 as Json5
 import qualified Data.ByteString as BS
+import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import System.Directory
 import System.FilePath.Posix
@@ -21,8 +22,18 @@ main = do
         env (BS.readFile (benchDir </> jsonFile)) $ \jsonBs ->
           bgroup
             jsonFile
-            [ bench "aeson-decode" $ nf (Aeson.eitherDecodeStrict :: BS.ByteString -> Either String Aeson.Value) jsonBs,
-              bench "parseJson" $ nf (doDecode >=> Json5.parseJson) jsonBs,
-              bench "parseJson5" $ nf (doDecode >=> Json5.parseJson5) jsonBs
+            [ bgroup
+                "from strict ByteString"
+                [ bench "aeson-decode" $ nf (Aeson.eitherDecodeStrict :: BS.ByteString -> Either String Aeson.Value) jsonBs,
+                  bench "parseJson" $ nf (doDecode >=> Json5.parseJson) jsonBs,
+                  bench "parseJson5" $ nf (doDecode >=> Json5.parseJson5) jsonBs
+                ],
+              env (either fail return (doDecode jsonBs)) $ \t ->
+                bgroup
+                  "from strict Text"
+                  [ bench "aeson-decode" $ nf (Aeson.eitherDecodeStrict . T.encodeUtf8 :: T.Text -> Either String Aeson.Value) t,
+                    bench "parseJson" $ nf Json5.parseJson t,
+                    bench "parseJson5" $ nf Json5.parseJson5 t
+                  ]
             ]
   defaultMain (map mkBenchForFile allFiles)
